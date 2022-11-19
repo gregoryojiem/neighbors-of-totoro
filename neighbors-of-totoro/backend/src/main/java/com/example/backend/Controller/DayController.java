e3ba8f8115c5cfd1d8c11ab004b3ba1965998627
Mildness Onyekwere <mno5865@rit.edu>
2022-11-19 15:12:50 -0500

    finished implmenting CRUD fro day, testing tbd

--- neighbors-of-totoro/backend/src/main/java/com/example/backend/Controller/DayController.java
+++ neighbors-of-totoro/backend/src/main/java/com/example/backend/Controller/DayController.java
@@ -1,4 +1,73 @@
 package com.example.backend.Controller;
 
+import com.example.backend.Model.Day;
+import com.example.backend.Service.DayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api")
 public class DayController {
+
+    @Autowired
+    private DayService dayService;
+
+    //CREATE
+    @CrossOrigin
+    @PostMapping(value = "/days", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Integer> createDay(@RequestBody Day newDay) {
+        int[] results = dayService.createDay(newDay);
+        if (results[0] == 1 && results[1] != 0) {
+            return new ResponseEntity<>(results[1], HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(results[1], HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+
+    //READ
+    @CrossOrigin
+    @GetMapping("/days/{dayID}")
+    public ResponseEntity<Day> getDay(@PathVariable UUID dayID) {
+        Day day = dayService.getDay(dayID);
+        return new ResponseEntity<>(day, HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @GetMapping("/days")
+    public ResponseEntity<List<Day>> getDays() {
+        List<Day> days = dayService.getDays();
+        return new ResponseEntity<>(days, HttpStatus.OK);
+    }
+
+    //UPDATE
+    @CrossOrigin
+    @PutMapping(value = "/days/{dayID}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Integer> updateAlbum(@PathVariable UUID dayID, @RequestBody Day dayDetails) {
+        int rowsAffected = dayService.updateDay(dayID, dayDetails);
+        if (rowsAffected == 1) {
+            return new ResponseEntity<>(rowsAffected, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(rowsAffected, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //DELETE
+    @CrossOrigin
+    @DeleteMapping("/albums/{dayID}")
+    public ResponseEntity<Integer> deleteAlbum(@PathVariable UUID dayID) {
+        int rowsAffected = dayService.deleteDay(dayID);
+        if (rowsAffected == 1) {
+            return new ResponseEntity<>(rowsAffected, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(rowsAffected, HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- neighbors-of-totoro/backend/src/main/java/com/example/backend/Service/DayService.java
+++ neighbors-of-totoro/backend/src/main/java/com/example/backend/Service/DayService.java
@@ -3,26 +3,36 @@ package com.example.backend.Service;
 import com.example.backend.Model.Day;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+@Service
 public class DayService {
     @Autowired
     DataSource dataSource;
 
     //CREATE
-    public Day createDay(Day day) {
-        String stmt = ("INSERT INTO day (start_time, end_time, date, timezone) VALUES()").formatted();
+    public int[] createDay(Day day) {
+        String stmt = ("insert into day (start_time, end_time, date, timezone) " +
+                "values('%tc', '%tc', '%tF', '%s')").formatted();
         Connection conn = DataSourceUtils.getConnection(dataSource);
         try {
             Statement statement = conn.createStatement(
                     ResultSet.TYPE_SCROLL_INSENSITIVE,
                     ResultSet.CONCUR_UPDATABLE);
-        } catch (Exception e)
-        {
+            int rowsAffected = statement.executeUpdate(stmt, Statement.RETURN_GENERATED_KEYS);
+            ResultSet keys = statement.getGeneratedKeys();
+            keys.next();
+            int key = keys.getInt(5);
+            return new int[]{rowsAffected, key};
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
@@ -31,12 +41,142 @@ public class DayService {
                 e.printStackTrace();
             }
         }
-        return new Day();
+        return new int[2];
     }
+
     //READ
+    public Day getDay(UUID dayID){
+        String query = ("select * from day where day_id='%s'").formatted(dayID);
+        Connection conn = DataSourceUtils.getConnection(dataSource);
+        try {
+            Statement statement = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = statement.executeQuery(query);
+            Day day = new Day();
+            while(rs.next()){
+                day.setDayID(rs.getObject("day_id", UUID.class));
+                day.setStartTime(rs.getTimestamp("start_time"));
+                day.setEndTime(rs.getTimestamp("end_time"));
+                day.setDate(rs.getDate("date"));
+                day.setTimeZone(rs.getString("timezone"));
+            }
+            return day;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public Day getDayByEvent(UUID eventID){
+        String query = ("").formatted(eventID);
+        Connection conn = DataSourceUtils.getConnection(dataSource);
+        try {
+            Statement statement = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = statement.executeQuery(query);
+            Day day = new Day();
+            while(rs.next()){
+                day.setDayID(rs.getObject("day_id", UUID.class));
+                day.setStartTime(rs.getTimestamp("start_time"));
+                day.setEndTime(rs.getTimestamp("end_time"));
+                day.setDate(rs.getDate("date"));
+                day.setTimeZone(rs.getString("timezone"));
+            }
+            return day;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public List<Day> getDays(){
+        String query = "select * from day";
+        Connection conn = DataSourceUtils.getConnection(dataSource);
+        try {
+            Statement statement = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = statement.executeQuery(query);
+            List<Day> days = new ArrayList<>();
+            while(rs.next()){
+                Day day = new Day();
+                day.setDayID(rs.getObject("day_id", UUID.class));
+                day.setStartTime(rs.getTimestamp("start_time"));
+                day.setEndTime(rs.getTimestamp("end_time"));
+                day.setDate(rs.getDate("date"));
+                day.setTimeZone(rs.getString("timezone"));
+                days.add(day);
+            }
+            return days;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
 
 
     //UPDATE
+    public int updateDay(UUID dayID, Day day) {
+        String stmt = ("update day set start_time='%tc', end_time='%tc', " +
+                "date='%tF', timezone='%s' where day_id='%s'").formatted(day.getStartTime(),
+                day.getEndTime(), day.getDate(), day.getTimeZone(), dayID);
+        Connection conn = DataSourceUtils.getConnection(dataSource);
+        try {
+            Statement statement = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            return statement.executeUpdate(stmt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return -1;
+    }
 
     //DELETE
+    public int deleteDay(UUID dayID) {
+        String stmt = ("delete from day where day_id='%s'").formatted(dayID);
+        Connection conn = DataSourceUtils.getConnection(dataSource);
+        try {
+            Statement statement = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+            return statement.executeUpdate(stmt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return -1;
+    }
 }
