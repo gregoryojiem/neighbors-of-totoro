8f1a10a17c1e465a4d6bdec8ad689a6846d850c7
Gregory Ojiem <gro3228@rit.edu>
2022-11-20 01:14:07 -0500

    implemented event.service and related data structures

---
+++ frontend/src/app/Day.ts
@@ -0,0 +1,7 @@
+export interface Day {
+  dayID: string,
+  timezone: string,
+  date: Date,
+  startTime: Date,
+  endTime: Date
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {EventService} from "./event.service";
+import { Event } from "./Event";
+import {TimeRange} from "./TimeRange";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private eventService : EventService) {
+    //var event: Event = {title:"a", description:"swobab", event_id:"1"}
+    //var timeRange: TimeRange = {startTime: new Date(3), endTime: new Date(3000000)}
+    //this.eventService.deleteEventHasDay("b88f8551-5d6e-452a-a20b-85f26521dc4e", "79430975-6e81-4c36-9afe-8d63cfaa0ae7").subscribe(info => {
+    //  console.log(info)
+    //})
+  }
+}
