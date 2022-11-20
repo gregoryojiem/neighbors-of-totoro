7bfcb855748f0f0d1ed75b77686a21c30e307566
Gregory Ojiem <gro3228@rit.edu>
2022-11-20 08:11:59 -0500

    finishd new meeting view

---
+++ frontend/src/app/new-meeting/new-meeting.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-new-meeting',
+  templateUrl: './new-meeting.component.html',
+  styleUrls: ['./new-meeting.component.css']
+})
+export class NewMeetingComponent implements OnInit {
+  ngOnInit(): void {
+  }
+  closeResult = '';
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: "lg", scrollable: true}).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      },
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+}
